09bffaaac9055352fab8fdd2dc566f9c2be56f9c
santosh ghode <santoshghode@santoshs-MacBook-Air.local>
2019-08-20 20:34:21 +0530

    spe

---
+++ build/precache-manifest.6351ba73174a4e123cc16fff5b21ac7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "159fb903228b0251e331ac5fb9d3530d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "074741b8669bfc4558d7",
+    "url": "/static/css/main.c32fecbd.chunk.css"
+  },
+  {
+    "revision": "23b8d791c9d5af0f2d31",
+    "url": "/static/js/2.766ef52f.chunk.js"
+  },
+  {
+    "revision": "074741b8669bfc4558d7",
+    "url": "/static/js/main.16a9aea2.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
